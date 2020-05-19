3d6c410f1f5b07fbcba839739582edf9d28bbc5d
keigo yunaga <ec2-user@ip-172-31-82-61.ec2.internal>
2020-05-19 10:39:59 +0000

    Lesson 03課題

---
+++ hello.rb
@@ -0,0 +1,27 @@
+# コメント
+
+=begin
+puts 改行
+p　デバック用
+変数　英子文字
+定数　英大文字
+警告は出るが処理は止まらない
+=end
+
+print "hello world"
+puts "hello world"
+p "hello world"
+
+#変数
+msg = "hello world"
+puts msg
+
+msg= "hello world again"
+puts msg
+
+#定数
+VERSION =1.1
+puts VERSION
+
+VERSION =1.2
+puts VERSION
